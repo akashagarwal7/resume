b9dc1b2935c2bdbb17b990c3ade64c7efeb5dd61
Akash Agarwal <virusofminds@gmail.com>
2020-07-08 13:33:41 +1000

    refactor: move export.sh to top level dir

--- export.sh
+++ export.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pandoc -s ./resume.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Resume\ \-\ Akash\ Agarwal.pdf
+pandoc -s ./resume.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Resume\ \-\ Akash\ Agarwal.docx
 pandoc -s ./cl.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Cover\ Letter\ \-\ Akash\ Agarwal.pdf
 pandoc -s ./cl.md -V urlcolor=blue -V geometry:margin=1in -t rst -o ./out/Cover\ Letter\ \-\ Akash\ Agarwal.txt
