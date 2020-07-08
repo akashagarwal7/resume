1b27a049c1972c462d708fa325f6bab3b406276a
Akash Agarwal <virusofminds@gmail.com>
2020-07-08 13:36:23 +1000

    fix: change export.sh paths

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pandoc -s ./resume.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Resume\ \-\ Akash\ Agarwal.pdf
-pandoc -s ./resume.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Resume\ \-\ Akash\ Agarwal.docx
-pandoc -s ./cl.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Cover\ Letter\ \-\ Akash\ Agarwal.pdf
-pandoc -s ./cl.md -V urlcolor=blue -V geometry:margin=1in -t rst -o ./out/Cover\ Letter\ \-\ Akash\ Agarwal.txt
+pandoc -s ./main/resume.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Resume\ \-\ Akash\ Agarwal.pdf
+pandoc -s ./main/resume.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Resume\ \-\ Akash\ Agarwal.docx
+pandoc -s ./main/cl.md -V urlcolor=blue -V geometry:margin=1in -o ./out/Cover\ Letter\ \-\ Akash\ Agarwal.pdf
+pandoc -s ./main/cl.md -V urlcolor=blue -V geometry:margin=1in -t rst -o ./out/Cover\ Letter\ \-\ Akash\ Agarwal.txt
